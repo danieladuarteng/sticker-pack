2af88b18ea0f626ff46f0c7ad74b2145a597287b
Daniela Duarte <dani@blanq.com.br>
2023-10-12 16:50:24 -0300

    fix(button): add aria-label on button to improve accessibility

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const Button = ({ disabledButton, onClick, icon, onBlur }) => {
+const Button = ({ dataTestid, ariaLabel, disabledButton, onClick, icon, onBlur }) => {
   return (
     <button
+      data-testid={dataTestid}
+      aria-label={ariaLabel}
       disabled={disabledButton}
       onClick={onClick}
       onBlur={onBlur}
